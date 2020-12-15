bb0b1b6ef42fc64d3d4ed2f1438d00a63752b133
Yu-jinKim <yujin.kim@hotmail.fr>
2020-12-15 14:31:07 +0000

    added checks for confidence levels and build

--- panelapp/Panelapp.py
+++ panelapp/Panelapp.py
@@ -219,6 +219,11 @@ class Panel():
             genes = []
 
             for level in confidence_levels:
+                assert level in [0, 1, 2, 3], (
+                    "Choose among the following levels: 3 (green), 2 (amber), "
+                    "1 (red), 0 (the rest)"
+                )
+
                 if str(level) in self.genes:
                     genes.append([
                         gene[key]
@@ -254,6 +259,11 @@ class Panel():
             genes = []
 
             for level in confidence_levels:
+                assert level in [0, 1, 2, 3], (
+                    "Choose among the following levels: 3 (green), 2 (amber), "
+                    "1 (red), 0 (the rest)"
+                )
+
                 if str(level) in self.genes:
                     genes.append(self.genes[str(level)])
 
@@ -300,6 +310,11 @@ class Panel():
         Returns:
             list: List of ensembl ids
         """
+
+        assert build in ["GRCh37", "GRCh38"], (
+            "Choose among 'GRCh37' and 'GRCh38' and then specify the "
+            "confidence levels"
+        )
 
         ensembl_ids = self.select_from_genes(
             "ensembl_id", *confidence_levels
